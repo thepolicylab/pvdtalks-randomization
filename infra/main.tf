11fed42d5fc83c3959db9076db1405263b64ba03
Kevin Wilson <khwilson@gmail.com>
2020-04-29 12:00:24 -0400

    Now with docker-compose fix

--- infra/main.tf
+++ infra/main.tf
@@ -8,12 +8,7 @@ resource "aws_key_pair" "auth" {
   public_key = file(var.public_key_path)
 }
 
-resource "aws_key_pair" "auth" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
-resource "aws_instance" "rishare-example-instance" {
+resource "aws_instance" "pvdtalks-instance" {
   connection {
     # Use local SSH for connection
     user = "ubuntu"  # Default value for our AMI
@@ -31,11 +26,19 @@ resource "aws_instance" "rishare-example-instance" {
   associate_public_ip_address = true
 
   # TODO (khw): For first deploy, here is where we'll actually do things
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sudo apt-get -y update",
-  #     "sudo apt-get install -y nginx",
-  #     "sudo service nginx start"
-  #   ]
-  # }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+      "sudo apt-get update",
+      "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+      "sudo curl -L \"https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose\"",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "git clone https://github.com/thepolicylab/pvdtalks-randomization",
+      "cd pvdtalks-randomization",
+      "docker-compose up -d"
+    ]
+  }
 }
